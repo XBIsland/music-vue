d4608db6477a94dc55697814a91fe9fd17c485a7
XBIsland <13726043109@139.com>
2018-10-12 00:15:04 +0800

    ip修改3

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -16,7 +16,7 @@ export function getRecommend () {
 
 export function getDiscList () {
   // 线上环境
-  const url = debug ? '/api/getDiscList' : '172.16.42.88/music/api/getDiscList'
+  const url = debug ? '/api/getDiscList' : '47.106.242.21/music/api/getDiscList'
 
   const data = Object.assign({}, commonParams, {
     platform: 'yqq',
@@ -38,7 +38,7 @@ export function getDiscList () {
 }
 
 export function getSongList (disstid) {
-  const url = debug ? '/api/getCdInfo' : '172.16.42.88/music/api/getCdInfo'
+  const url = debug ? '/api/getCdInfo' : '47.106.242.21/music/api/getCdInfo'
 
   const data = Object.assign({}, commonParams, {
     disstid,
--- src/api/song.js
+++ src/api/song.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 import { ERR_OK } from 'api/config'
 
 export function getLyric (mid) {
-  const url = debug ? '/api/lyric' : '172.16.42.88/music/api/lyric'
+  const url = debug ? '/api/lyric' : '47.106.242.21/music/api/lyric'
 
   const data = Object.assign({}, commonParams, {
     songmid: mid,
@@ -24,7 +24,7 @@ export function getLyric (mid) {
 }
 
 export function getSongsUrl (songs) {
-  const url = debug ? '/api/getPurlUrl' : '172.16.42.88/music/api/getPurlUrl'
+  const url = debug ? '/api/getPurlUrl' : '47.106.242.21/music/api/getPurlUrl'
 
   let mids = []
   let types = []
